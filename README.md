05254c224c770534871eca04be70c5232ac50a06
Anton B <tonborzenko@gmail.com>
2021-11-21 17:41:25 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+## Start a project
+
+```bash
+# Install dependencies
+yarn install
+
+# Start sveltekit app
+yarn dev
+```
---
+++ src/routes/index.svelte
@@ -0,0 +1,20 @@
+<script>
+  import { onMount } from 'svelte'
+  import * as nearAPI from 'near-api-js'
+  let near;
+
+  onMount(async() => {
+    const config = {
+      networkId: "testnet",
+      nodeUrl: "https://rpc.testnet.near.org",
+      walletUrl: "https://wallet.testnet.near.org",
+      helperUrl: "https://helper.testnet.near.org",
+      explorerUrl: "https://explorer.testnet.near.org",
+      keyStore: new nearAPI.keyStores.BrowserLocalStorageKeyStore()
+    };
+
+    near = await nearAPI.connect(config);
+  })
+</script>
+
+{JSON.stringify(near)}
